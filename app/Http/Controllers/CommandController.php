664a81494ddbb15ec46888a8e73efa2b89cf181b
Ivan Volynets <sobwoofer8@gmail.com>
2020-10-23 11:40:21 +0300

    test

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Symfony\Component\Process\Process;
+use Log;
 
 class CommandController extends Controller
 {
@@ -26,14 +27,23 @@ class CommandController extends Controller
             return back()->with('error', $e->getMessage());
         }
 
-        return back()->with('success', 'Command "sync-products" run successfully');
+//        return back()->with('success', 'Command "sync-products" run successfully');
     }
 
     private function runProcess($command)
     {
+        Log::info('started runProcess in controller'. $command);
+
         $phpPath = config('filesystems.php_path');
         $cwdPath = config('filesystems.cwd_path');
         $process = new Process($phpPath . ' artisan ' . $command . ' > /dev/null 2>&1 &', $cwdPath);
         $process->start();
+        $process->wait(function ($type, $buffer) {
+            if (Process::ERR === $type) {
+                echo 'ERR > '.$buffer;
+            } else {
+                echo 'OUT > '.$buffer;
+            }
+        });
     }
 }
